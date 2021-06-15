438786af29ce553c5a1f164cb9ca79f2c3cb0e98
Yunusxon Egamberdiev <yunusxon@gmail.com>
2021-06-15 18:41:21 +0500

    add

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDeail from "./comment-detail";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+	return (
+		<div className="ui container comments">
+			<CommentDeail author="Sam" />
+			<CommentDeail author="Alex" />
+			<CommentDeail author="Jane" />
+			<CommentDeail author="Josh" />
+			<CommentDeail author="Lynda" />
+			<CommentDeail author="Bob" />
+		</div>
+	);
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"));
